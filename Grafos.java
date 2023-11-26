ccebef02335be0cd28168e50597690157368ae7c
IssPame19 <issabelpamela4e@gmail.com>
2023-11-26 04:42:01 -0300

    Algunos metodos realizados

--- Grafos.java
+++ Grafos.java
@@ -1,9 +1,186 @@
+import java.util.*;
+
 public class Grafos {
+    int Vertices; // Numero de vertices
+    ArrayList<ArrayList<Integer>> ListaAdy; // Lista de adyacencia
+    boolean[] Visitados; // Para marcar los vertices visitados
+    LinkedList<Integer> Cola; // Se crea una cola que almacena los nodos a visitar
+    Stack<Integer> Pila; // Se crea una pila para los vertices a visitar
+
+    // Contructor
+    public Grafos(int vertices) {
+        Vertices = vertices;
+        ListaAdy = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            ListaAdy.add(new ArrayList<>());
+        }
+        Visitados = new boolean[vertices];
+        Cola = new LinkedList<>();
+        Pila = new Stack<>();
+    }
+
+    // Agregar Arista A Los Grafos; v = Vertice Origen; w = Vertice Destino
+    public void AgregarArista(int v, int w) {
+        // Agrega las aristas a la lista de adyacencia
+        ListaAdy.get(v).add(w);
+    }
+
     // Metodos = Algoritmos
+    public void EmparejamientoPerfecto(int S) {
+        // Se inicia un array para almacenar emparejamientos
+        // El -1 indica que no hay emparezamientos para ese vertice
+        int[] Emparejamiento = new int[Vertices];
+        Arrays.fill(Emparejamiento, -1);
+
+        // Clase interna para DFS
+        class DFS {
+            boolean Dfs(int vertice, int[] emparejamiento) {
+                // Recorremos todos los vertices adyacentes al actual
+                for (int i : ListaAdy.get(vertice)) {
+                    if (emparejamiento[i] == -1) {
+                        // Emparejamos el vertice actual con el adyacente
+                        emparejamiento[i] = vertice;
+                        emparejamiento[vertice] = i;
+                        // Se indica que se ha encontrado el emparejamiento
+                        return true;
+                    }
+                }
+
+                // Si no se encontro un emparejamiento se busca para los vertices adyacentes
+                for (int j : ListaAdy.get(vertice)) {
+                    if (Dfs(Emparejamiento[j], Emparejamiento)) {
+                        // Emparejamos el vertice actual con el adyacente
+                        emparejamiento[vertice] = j;
+                        emparejamiento[j] = vertice;
+                        // Se indica que se ha encontrado el emparejamiento
+                        return true;
+                    }
+                }
+
+                // Si no se encontro emparejamiento se devuelve falso
+                return false;
+            }
+        }
+
+        // Se instancia la clase DFS
+        DFS dfs = new DFS();
+
+        // Se recorren todos los vertices
+        for (int i = 0; i < Vertices; i++) {
+            if (Emparejamiento[i] == -1) {
+                // Se intenta encontrar un emparejamiento
+                if (!dfs.Dfs(i, Emparejamiento)) {
+                    // Si no se logra encontrar un emparejamiento
+                    System.out.println("No se pudo encontrar un emparejamiento perfecto");
+                }
+            }
+        }
+
+        // Si se encontro un emparejamiento y se muestra
+        System.out.println("Se ha encontrado un emparejamiento perfecto");
+        for (int i = 0; i < Vertices; i++) {
+            System.out.println(i + " -> " + Emparejamiento[i]);
+        }
+    }
+
+    public void TMA_MatrimonioEstable() {
+    }
+
+    public void BFS_BusquedaEnAmplitud(int S) {
+        // Se marcan todos los vertices como no visitados
+        for (int i = 0; i < Vertices; i++) {
+            Visitados[i] = false;
+        }
+
+        // Se marca el nodo actual como visitado y se pone en la cola
+        Visitados[S] = true;
+        Cola.add(S);
+
+        // Se ejecuta mientras haya nodos en la cola
+        while (Cola.size() != 0) {
+            // Se saca de la cola el nodo y se imprime
+            S = Cola.poll();
+            System.out.print(S + " ");
+
+            // Se obtienen los vertices adyacentes del vertice S
+            Iterator<Integer> i = ListaAdy.get(S).listIterator();
+            // Se ejecuta mientras haya nodos adyacentes
+            while (i.hasNext()) {
+                // Obtiene los nodos adyacentes
+                int VericeAdy = i.next();
+                // Si el nodo adyacente no ha sido visitado
+                if (!Visitados[VericeAdy]) {
+                    // Marca el nodo como visitado y lo agrega a la cola
+                    Visitados[VericeAdy] = true;
+                    Cola.add(VericeAdy);
+                }
+            }
+        }
+    }
+
+    public void DFS_BusquedaEnProfundidad(int S) {
+        // Se agrega el vertice al inicio y se marca como visitado
+        Pila.push(S);
+        Visitados[S] = true;
+
+        // Funciona mientras la pila no este vacia
+        while (!Pila.isEmpty()) {
+            // Se toma el vertice superior y se imprime
+            int Actual = Pila.pop();
+            System.out.print(Actual + " ");
+
+            // Para los vertices adyacentes al actual
+            for (int Adyacentes : ListaAdy.get(Actual)) {
+                // Si el adyacente no ha sido visitado
+                if (!Visitados[Adyacentes]) {
+                    // Se agrega a la pila y se marca como visitado
+                    Pila.push(Adyacentes);
+                    Visitados[Adyacentes] = true;
+                }
+            }
+        }
+    }
+
+    public void Dijkstra() {
+    }
+
+    public void Forward() {
+    }
+
+    public void BellmanFord() {
+    }
+
+    public void DAG_OrdenamientoTopologico() {
+        // Se recorren todos los vertices
+        for (int i = 0; i < Vertices; i++) {
+            // Si el verice no se ha visitado
+            if (!Visitados[i]) {
+                Visitados[i] = true;
+                // Se recorren los vertices adyacentes al actual
+                for (int j = 0; j < ListaAdy.get(i).size(); j++) {
+                    int Ady = ListaAdy.get(i).get(j);
+                    // Si el verice no se ha visitado
+                    if (!Visitados[Ady]) {
+                        DAG_OrdenamientoTopologico();
+                    }
+                }
+
+                // Si no hay vertices adyacentes no visitados, se agregan a la Pila y se imprime
+                Pila.push(i);
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public void Kruskal() {
+    }
+
+    public void MST_ArbolDeExpansionMinma() {
+    }
 
     public static void main(String[] args) {
         // Recibe el grafo
-        
+        // Menu
         // If con codiciones pra uso de metodos
     }
 }
